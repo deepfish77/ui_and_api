8e39499c35ed8be2d795989a6ba90a5ac7a43fc8
shachar-czitron <shachar@amenityanalytics.com>
2025-02-17 23:10:30 +0200

    test

--- src/ui_tests/utils/drivers.py
+++ src/ui_tests/utils/drivers.py
@@ -3,10 +3,14 @@ from selenium import webdriver
 PATH_TO_DRIVER = "C:\\AutoCourse\\practiceback\\drivers\\chromedriver.exe"
 
 
-def _get_mobile_emulation_options(device_name="iPhone X"):
+def _get_mobile_emulation_options(device_name="Samsung Galaxy S10"):
 
     chrome_options = webdriver.ChromeOptions()
-    mobile_emulation = {"deviceName": device_name}
+    mobile_emulation = {
+        "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
+        "userAgent": "Mozilla/5.0 (Linux; Android 8.1.0; Pixel 2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Mobile Safari/537.36",
+    }
+
     chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
     return chrome_options
 
